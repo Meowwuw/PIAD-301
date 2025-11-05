841a1ce3c6ba3a1b88a58cc2d93bed16e5500a96
Magenta Paredes <paredesponcemagenta@gmail.com>
2025-11-05 10:51:27 -0500

    Feat: Mejora para el login tradicional con LocalStorage

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
 import Login from "./pages/Login";
 import LoginSuccess from "./pages/LoginSuccess";
 import LoginError from './pages/LoginError';
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/login-error" element={<LoginError />} />
         <Route path="/login-success" element={<LoginSuccess />} />
--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -3,9 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        primary: '#46c768', 
+        secondary: '#ff7849',
+      },
       fontFamily:{
         sans: ["Bitcount Grid Single", "sans-serif"]
-      }
+      },
     },
   },
   plugins: [],
