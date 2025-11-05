b8930bb7709fff95668c2de0befb27cb48b348c9
Magenta Paredes <paredesponcemagenta@gmail.com>
2025-11-05 09:50:20 -0500

    Feat: Enviando servicio de Login con registro tradicional

--- src/backend/routes/authRoutes.js
+++ src/backend/routes/authRoutes.js
@@ -46,6 +46,35 @@ const router = express.Router();
  */
 router.post("/register", authControllers.register);
 
+/**
+ * @swagger
+ * /api/auth/login:
+ *  post:
+ *    summary: Iniciar sesion del usuario
+ *    tags: [Auth]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *                example: luzclarita@gmail.com
+ *              password:
+ *                type: string
+ *                example: 123
+ *    responses:
+ *      200:
+ *        description: Inicio de sesion exitoso
+ *      401:
+ *        description: Credenciales invalidas
+ *      500:
+ *        description: Error interno del servidor
+ */
+router.post("/login", authControllers.login);
+
 router.get(
   "/google",
   passport.authenticate("google", {
